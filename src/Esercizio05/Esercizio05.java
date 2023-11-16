f83034bdab50bcd195d9a128323d07c0b89d7b4d
Saveriodinapoli19 <saveriodinapoli99@gmail.com>
2023-11-16 19:33:50 +0100

    Esercizio05

--- src/Esercizio05/Esercizio05.java
+++ src/Esercizio05/Esercizio05.java
@@ -5,23 +5,23 @@ Usare la funzione appena scritta in due blocchi di codice
 dove si cattura l'eventuale eccezione.*/
 public class Esercizio05 {
     public static void main(String[] args) {
-        Nullability(null,2);
-    }
-    public static boolean Nullability(Integer number, Integer denominator){
         try {
-            if (number == null || denominator == null) {
-                System.out.println("Number or denominator must be specified");
-            } else{
-                int result = number / denominator;
-                System.out.println(result);
-
-            }
-
+            Integer number = 5;
+            Integer denominator= null;
+            nullability(number,denominator);
         }catch(Exception e){
             throw new ArithmeticException("Error");
         }
+    }
+    public static boolean nullability(Integer number, Integer denominator){
+        if (number == null || denominator == null) {
+            System.out.println("Number or denominator must be specified");
+        } else{
+            int result = number / denominator;
+            System.out.println(result);
 
-//non so se è fatto bene e se ho seguito la traccia, ma vorrei sapere se il modo usato è corretto.
+        }
+
 
         return false;
     }
